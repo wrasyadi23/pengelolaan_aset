b5db428e7049a7cdac0b1160af5ea6d0dc590e3c
wrasyadi23 <muhammad.rasyadi23@gmail.com>
2020-03-21 00:09:03 +0700

    buat layout dan side bar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('level');
+            $table->string('sub_level');
             $table->rememberToken();
             $table->timestamps();
         });
