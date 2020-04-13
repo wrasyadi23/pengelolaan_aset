0813816de82a0b4467bf3fc3722de3dfdde16d82
wrasyadi23 <muhammad.rasyadi23@gmail.com>
2020-04-13 16:46:47 +0700

    store

--- app/Http/Controllers/input_pekerjaanController.php
+++ app/Http/Controllers/input_pekerjaanController.php
@@ -23,12 +23,46 @@ class input_pekerjaanController extends Controller
     }
 
     public function store(Request $request) {
-        // input form 
+        // get book number
+        $data = Pekerjaan::select('id','bookNumber','YEAR(tanggal_pekerjaan)')
+        ->where('YEAR(tanggal_pekerjaan)','=',date('Y'))
+        ->orderBy('id','desc')->count();
+        $tahun_sekarang = date('Y m');
+        if ($data > 0) {
+            $getBookNumber = $tahun_sekarang . sprintf('%05s', $data + 1);
+        }
+        else {
+            $getBookNumber = $tahun_sekarang . sprintf('%05s', 1);
+        }
+
+        $bookNumber = $getBookNumber;
         $nama = $request->input('nama');
         $nik = $request->input('nik');
         $kd_klasifikasi_pekerjaan = $request->input('kd_klasifikasi_pekerjaan');
         $tanggal_pekerjaan = date('Y-m-d');
         $uraian = $request->input('uraian');
-        $file = $request->file('file');
+        $file = $request->file('file')->move('',$request->file('file')->getClientOriginalName());
+
+        // menghitung pekerjaan dari data hari terakhir
+        $validasi_tanggal_pelaksanaan = Pekerjaan::select('id','tanggal_pelaksanaan')
+        ->orderBy('id','desc')->first();
+        if ($validasi_tanggal_pelaksanaan->tanggal_pelaksanaan->count() <= 5) {
+            $tanggal_pelaksanaan = date('Y-m-d');
+        }
+        else {
+            $tanggal_pelaksanaan = date('Y-m-d') + 1;
+        }
+
+        $pekerjaan = new Pekerjaan;
+        $pekerjaan->bookNumber = $bookNumber;
+        $pekerjaan->nama = $nama;
+        $pekerjaan->nik = $nik;
+        $pekerjaan->kd_klasifikasi_pekerjaan = $kd_klasifikasi_pekerjaan;
+        $pekerjaan->tanggal_pekerjaan = $tanggal_pekerjaan;
+        $pekerjaan->tanggal_pelaksanaan = $tanggal_pelaksanaan;
+        $pekerjaan->uraian = $uraian;
+        $pekerjaan->file = $request->file('file')->getClientOriginalName();
+        $pekerjaan->status = '1';
+
     }
 }
