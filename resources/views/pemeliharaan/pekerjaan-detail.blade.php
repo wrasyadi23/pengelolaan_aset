069937cfc1b448059de71791bb74b9a7b887d477
wrasyadi23 <muhammad.rasyadi23@gmail.com>
2020-04-23 17:15:56 +0700

    #

--- resources/views/pemeliharaan/pekerjaan-detail.blade.php
+++ resources/views/pemeliharaan/pekerjaan-detail.blade.php
@@ -9,7 +9,7 @@
                     <h4 class="card-title">Detail Pekerjaan</h4>
                     <div class="card-content">
                         {{-- Tampilan detail table, uncomment dari bawah untuk pakai --}}
-                        <table class="table table-hover table-bordered table-striped">
+                        <table class="table table-bordered table-striped">
                             <tr>
                                 <th>Book Number</th>
                                 <td>{{$DetailPekerjaan->booknumber}}</td>
@@ -34,46 +34,11 @@
                                 <th>Uraian</th>
                                 <td>{{$DetailPekerjaan->uraian}}</td>
                             </tr>
+                            <tr>
+                                <th>Foto</th>
+                                <td><a href="{{asset('pemeliharaan/'.$DetailPekerjaan->file)}}">{{$DetailPekerjaan->file}}</a></td>
+                            </tr>
                         </table>
-
-                        {{-- Tampilan form, uncomment dari bawah untuk pakai  --}}
-                        {{-- <div class="basic-form">
-                            <div class="form-group row">
-                                <label for="booknumber" class="col-sm-3 col-form-label">Book Number</label>
-                                <div class="col-sm-9">
-                                    <input type="text" name="" id="" class="form-control-plaintext" value="{{$DetailPekerjaan->booknumber}}" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="booknumber" class="col-sm-3 col-form-label">Nama / NIK</label>
-                                <div class="col-sm-9">
-                                    <input type="text" name="" id="" class="form-control-plaintext" value="{{$DetailPekerjaan->nama}} / {{$DetailPekerjaan->nik}}" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="booknumber" class="col-sm-3 col-form-label">Klasifikasi Pekerjaan</label>
-                                <div class="col-sm-9">
-                                    <input type="text" name="" id="" class="form-control-plaintext" value="{{$DetailPekerjaan->kd_klasifikasi_pekerjaan}} - {{$DetailPekerjaan->getKlasifikasi->klasifikasi_pekerjaan}}" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="booknumber" class="col-sm-3 col-form-label">Tanggal</label>
-                                <div class="col-sm-9">
-                                    <input type="text" name="" id="" class="form-control-plaintext" value="{{$DetailPekerjaan->tanggal_pekerjaan}}" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="booknumber" class="col-sm-3 col-form-label">Jadwal Sementara</label>
-                                <div class="col-sm-9">
-                                    <input type="text" name="" id="" class="form-control-plaintext" value="{{$DetailPekerjaan->tanggal_pelaksanaan}}" readonly>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="booknumber" class="col-sm-3 col-form-label">Uraian</label>
-                                <div class="col-sm-9">
-                                    <textarea name="" id="" rows="7" class="form-control-plaintext" readonly>{{$DetailPekerjaan->uraian}}</textarea>
-                                </div>
-                            </div> --}}
                         </div>
                     </div>
                 </div>
