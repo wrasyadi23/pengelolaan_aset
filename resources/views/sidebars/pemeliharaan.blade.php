82e7ed48560a4914baf55b5634cd0713590bc880
wrasyadi23 <muhammad.rasyadi23@gmail.com>
2020-05-05 22:05:25 +0700

    #

--- resources/views/sidebars/pemeliharaan.blade.php
+++ resources/views/sidebars/pemeliharaan.blade.php
@@ -21,7 +21,7 @@
                 <i class="icon-chart menu-icon"></i><span class="nav-text">Data Pemeliharaan</span>
             </a>
         </li>
-        <li>
+        {{-- <li>
             <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                 <i class="icon-graph menu-icon"></i><span class="nav-text">Suku Cadang</span>
             </a>
@@ -30,7 +30,7 @@
                 <li><a href="/pemeliharaan/suku-cadang-info">Info Stock</a></li>
                 <li><a href="/pemeliharaan/suku-cadang-laporan">Laporan</a></li>
             </ul>
-        </li>
+        </li> --}}
         <li>
             <a href="/pemeliharaan/laporan" aria-expanded="false">
                 <i class="icon-notebook menu-icon"></i><span class="nav-text">Laporan</span>
