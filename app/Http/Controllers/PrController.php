6cc4a80ac24c4afe8fb877a51639824bef908522
gunpetro10 <gun.petro10@gmail.com>
2020-11-12 20:28:38 +0700

    edit pr-tampil

--- app/Http/Controllers/PrController.php
+++ app/Http/Controllers/PrController.php
@@ -1,69 +1,60 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Sr;
 use App\PR;
 use Illuminate\Http\Request;
-
 class PrController extends Controller
 {
-    public function create()
-    {
+    public function create(){
         $rawDataSR = SR::orderBy('id', 'desc')
-            ->where('status', 'Request')
+            ->where('status','Request')
             ->get();
         return view('transport/pr-create', [
             'rawDataSR' => $rawDataSR
-        ]);
+            ]);
     }
 
     public function store(Request $request)
     {
-        $data = PR::select('id', 'kd_pr', 'tgl')
-            ->whereYear('tgl', date('Y'))
-            ->orderBy('id', 'desc')->count();
-        $tahun_sekarang = date('Ym');
-        if ($data > 0) {
-            $kd_pr = 'PR' . $tahun_sekarang . sprintf('%05s', $data + 1);
-        } else {
-            $kd_pr = 'PR' . $tahun_sekarang . sprintf('%05s', 1);
-        }
+            $data = PR::select('id', 'kd_pr', 'tgl')
+                ->whereYear('tgl', date('Y'))
+                ->orderBy('id', 'desc')->count();
+            $tahun_sekarang = date('Ym');
+            if ($data > 0) {
+                $kd_pr = 'PR' . $tahun_sekarang . sprintf('%05s', $data + 1);
+            } else {
+                $kd_pr = 'PR' . $tahun_sekarang . sprintf('%05s', 1);
+            }
+    
+            $tgl = $request->input('tgl');
+            $kd_sr = $request->input('kd_sr');
+            $no_pr = $request->input('no_pr');
+            
+            $newRealisasi = new PR();
+            $newRealisasi->kd_pr = $kd_pr;
+            $newRealisasi->no_pr = $no_pr;
+            $newRealisasi->tgl = $tgl;
+            $newRealisasi->kd_sr = $kd_sr;
+            $newRealisasi->save();
 
-        $tgl = $request->input('tgl');
-        $kd_sr = $request->input('kd_sr');
-        $no_pr = $request->input('no_pr');
-
-        $newRealisasi = new PR();
-        $newRealisasi->kd_pr = $kd_pr;
-        $newRealisasi->no_pr = $no_pr;
-        $newRealisasi->tgl = $tgl;
-        $newRealisasi->kd_sr = $kd_sr;
-        $newRealisasi->save();
-
-        $newRealisasi = SR::where('kd_sr', $kd_sr)->first();
-        $newRealisasi->status = 'PR';
-        $newRealisasi->save();
-
-        return redirect('transport/pr-tampil');
+            $newRealisasi = SR::where('kd_sr', $kd_sr)->first();
+            $newRealisasi->status = 'PR';
+            $newRealisasi->save();
+                    
+            return redirect('transport/pr-tampil');
     }
 
-    public function cari(Request $data)
-    {
+    public function cari(Request $data){
         $key = $data->key;
-        $pr = PR::where('no_pr', 'like', "%" . $key . "%")
-            ->paginate(10);
+        $pr = PR::where('no_pr','like',"%".$key."%")
+        ->paginate(10);
         return view('transport/pr-tampil', ['pr' => $pr]);
     }
 
-    public function tampil()
-    {
-        $pr = PR::with(['getSr.getKontrakBa.getKendaraan' => function ($query) {
-            return $query->whereNotNull('merk');
-        }])
-            ->orderBy('id', 'desc')
-            ->paginate(10);
-        dd($pr);
+    public function tampil(){
+        $pr = PR::orderBy('id', 'desc')
+        ->paginate(10);
         return view('transport/pr-tampil', ['pr' => $pr]);
     }
 
@@ -82,7 +73,7 @@ class PrController extends Controller
         $newRealisasi->no_pr = $no_pr;
         $newRealisasi->tgl = $tgl;
         $newRealisasi->save();
-
+                
         return redirect('transport/pr-tampil');
     }
 }
--- resources/views/transport/pr-tampil.blade.php
+++ resources/views/transport/pr-tampil.blade.php
@@ -3,7 +3,7 @@
     .style2 {color: #000000}
     </style>
     @extends('layouts.master')
-    @section('title','Data SR, PR Sewa Kendaraan')
+    @section('title','Data Service Request Transport')
     @section('content')
     <div class="container-fluid mt-3">
         @if (session('message'))
@@ -13,22 +13,18 @@
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title style1">Data SR, PR Sewa Kendaraan</h4>
-                        <form action="/transport/pr-cari" method="get">
-                            <div class="row">
-                                <div class="form-group col-md-8">
-                                    <input type="text" name="key" id="" class="form-control" placeholder="Panggil Purchase Request (No.PR)">
-                                </div>
-                                <div class="form-group col-md-4">
-                                    <button type="submit" class="btn btn-primary">Cari</button>
-                                    <button type="reset" class="btn btn-primary" onclick="window.location.href='/transport/pr-tampil'">Reset</button>
-                                    <a href="/transport/pr-create" class="btn btn-success">PR Baru</a>
-                                </div> 
-                        </form>
+                        <div class="form-row">
+                            <div class="col-md-6">
+                                <h4 class="card-title style1">Data Service Request Transport</h4>
+                            </div>
+                            <div class="col-md-6 text-right">
+                                <button class="btn btn-primary" onclick="window.location.href='/transport/pr-create'">+ PR Baru</button>
+                            </div>
+                        </div>
                         <div class="card-content">
                             {{-- Tampilan detail table, uncomment dari bawah untuk pakai --}}
                             <div class="table-responsive">
-                                <table class="table table-bordered table-striped">
+                                <table class="table table-bordered table-striped zero-configuration" style="width: 100%;">
                                     <thead>
                                         <tr class = "table-secondary">
                                             <th><div align="center">No</div></th>
@@ -39,35 +35,47 @@
                                             <th><div align="center">Mulai Sewa</div></th>
                                             <th><div align="center">Sewa Sampai</div></th>
                                             <th><div align="center">Uraian</div></th>
-                                            <th><div align="center">Jumlah</div></th>
-                                            <th><div align="center">Satuan</div></th>
                                             <th><div align="center">Rekanan</div></th>
+                                            <th><div align="center">Jenis Sewa</div></th>
                                             <th><div align="center">Aksi</div></th>
                                         </tr>
                                     </thead>
                                     <tbody>
+                                        @php
+                                            $no = 1;
+                                        @endphp
                                         @foreach ($pr as $result => $r)
                                         <tr>
-                                            <td>{{ $result + $pr->firstitem() }}</td>
-                                            <td>{{ $r->no_pr }}</td>
-                                            <td>{{ $r->tgl }}</td>
-                                            <td>{{ $r->getSR->no_sr }}</td>
-                                            <td>{{ $r->getSR->tgl }}</td>
-                                            <td>{{ $r->getSR->tgl_awal }}</td>
-                                            <td>{{ $r->getSR->tgl_akhir }}</td>                                    
-                                            <td>{{ $r->getSR->getKontrakBA->getKontrak->uraian }}</td>
-                                            <td>{{ $r->getSR->getKontrakBA->getKontrak->jml }}</td>
-                                            <td>{{ $r->getSR->getKontrakBA->getKontrak->satuan }}</td>
-                                            <td>{{ $r->getSR->getKontrakBA->getKontrak->rekanan }}</td>
+                                            <td><div align="center">{{ $result + $pr->firstitem() }}</div></td>
+                                            <td><div align="center">{{ $r->no_pr }}</div></td>
+                                            <td><div align="center">{{ $r->tgl }}</div></td>
+                                            <td><div align="center">{{ $r->getSR->no_sr }}</div></td>
+                                            <td><div align="center">{{ $r->getSR->tgl }}</div></td>
+                                            <td><div align="center">{{ $r->getSR->tgl_awal }}</div></td>
+                                            <td><div align="center">{{ $r->getSR->tgl_akhir }}</div></td>                                    
+                                            @if ( $r->getSR->getKontrakBA->no_ba == "Sewa-Esidentil")
+
+                                            @php
+                                            $tglAwal[$result] = \Carbon\Carbon::parse($r->tgl_awal);
+                                            $tglAkhir[$result] = \Carbon\Carbon::parse($r->tgl_akhir);
+                                            $hari[$result] = $tglAwal[$result]->diffInDays($tglAkhir[$result]) + 1;
+                                            @endphp
+                                                
+                                            <td><div align="left"><span class="style3">Sewa Kendaraan {{ $r->getSR->getKontrakBA->getKendaraan->jenis_kend }} {{ $r->getSR->getKontrakBA->getKendaraan->merk }}  {{ date('Y', strtotime($r->getSR->tgl)) }} - {{$hari[$result]}} {{ $r->getSR->getKontrakBA->getKontrak->satuan }}</span></div></td>
+                                            @else
+                                            <td><div align="left">{{ $r->getSR->getKontrakBA->getKontrak->uraian }} {{ $r->getSR->getKontrakBA->getKendaraan->merk }} {{ date('Y', strtotime($r->tgl)) }} - {{ $r->getSR->getKontrakBA->getKontrak->jml }} {{ $r->getSR->getKontrakBA->getKontrak->satuan }}</div></td>
+                                            @endif
+                                           
+                                            <td><div align="center">{{ $r->getSR->getKontrakBA->getKontrak->rekanan }}</div></td>
+                                            <td><div align="center">{{ $r->getSR->getKontrakBA->getKendaraan->jenis_sewa}}</div></td>
+                                          
                                             <th>
-                                                <a href="/transport/pr-edit/{{ $r->id }}" class="badge badge-primary">Edit</a>
-                                            </th>
+                                                <a href="/transport/pr-edit/{{ $r->id }}" class="badge badge-primary">Edit</a>                                            </th>
                                         </tr>
                                         @endforeach
                                     </tbody>
                                 </table>
                             </div>
-                            
                         </div>
                     </div>
                 </div>
