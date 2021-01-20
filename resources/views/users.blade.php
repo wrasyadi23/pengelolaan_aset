de42874ba0e301c9ebc9205116ce2db4fb79b172
wrasyadi23 <muhammad.rasyadi23@gmail.com>
2021-01-20 22:48:25 +0700

    update: usercontroller->store, user.blade->add field organisasi;

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -81,9 +81,21 @@
                                                         class="form-control input-default" required>
                                                 </div>
                                                 <div class="form-group">
-                                                    <label for="password">Password</label>
-                                                    <input type="password" name="password" id="" 
-                                                        class="form-control input-default" required>
+                                                    <label for="bagian">Bagian</label>
+                                                    <select name="kd_bagian" id="kd_bagian" class="form-control input-default" required>
+                                                        <option disabled selected></option>
+                                                        @foreach ($bagian as $itemBagian)
+                                                            <option value="{{$itemBagian->kd_bagian}}">{{$itemBagian->bagian}}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="seksi">Seksi</label>
+                                                    <select name="kd_seksi" id="kd_seksi" class="form-control input-default" disabled required></select>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="regu">Regu</label>
+                                                    <select name="kd_regu" id="kd_regu" class="form-control input-default" required></select>
                                                 </div>
                                                 <div class="form-group">
                                                     <label for="">Role</label>
@@ -133,7 +145,7 @@
                                         </button>
                                     </div>
                                     <div class="modal-body">
-                                        <form action="users-import"
+                                        <form action="/users-import"
                                               method="post" name="import" enctype="multipart/form-data">
                                             @csrf
                                             <div class="basic-form">
@@ -170,5 +182,76 @@
             placeholder: 'Pilih Level Group',
             allowClear: true
         });
+        $("#kd_bagian").select2({
+            placeholder: 'Pilih Bagian',
+            allowClear: true
+        }); // fungsi untuk mengubah dropdown biasa menjadi plugin select2
+        $("#kd_seksi").select2({
+            placeholder: 'Pilih Seksi',
+            allowClear: true,
+            disabled: true
+        }); // fungsi untuk mengubah dropdown biasa menjadi plugin select2
+        $("#kd_regu").select2({
+            placeholder: 'Pilih Regu',
+            allowClear: true,
+            disabled: true
+        }); // fungsi untuk mengubah dropdown biasa menjadi plugin select2
+
+
+        $("#kd_bagian").change(function () {
+            var seksi = "<option disabled selected></option>"
+            $("#kd_seksi")
+                .empty()
+                .prop("disabled", true);
+            $("#kd_regu")
+                .empty()
+                .prop("disabled", true);
+            $.ajax({
+                type: "POST",
+                url: "/api/get-bagian", // memanggil url di controller API/Controller/GetResponse@getAlamat & akan output data JSON
+                data: {
+                    kd_bagian: $("#kd_bagian").val()
+                },
+                error: function(e) {
+                    console.log(e)
+                },
+                success: function(response) {
+                    var data = JSON.parse(response);
+                    for (var x = 0; data.length > x; x++) {
+                        seksi += "<option value="+data[x].kd_seksi + ">" + data[x].seksi + "</option>"; // data json yang telah dioutput diassign ke variable dalam bentuk tag <option>
+                    }
+                    console.log(seksi); // ini hanya untuk cek di console browser, apakah data berhasil teroutput?
+                    $("#kd_seksi")
+                    .empty()
+                    .append(seksi) // variable yang berisi tag <option> diassign ke combobox terkait
+                    .prop("disabled", false);
+                }
+            })
+        })
+
+        $("#kd_seksi").change(function () {
+            var regu = "<option disabled selected></option>"
+            $.ajax({
+                type: "POST",
+                url: "/api/get-seksi", // memanggil url di controller API/Controller/GetResponse@getAlamat & akan output data JSON
+                data: {
+                    kd_seksi: $("#kd_seksi").val()
+                },
+                error: function(e) {
+                    console.log(e)
+                },
+                success: function(response) {
+                    var data = JSON.parse(response);
+                    for (var x = 0; data.length > x; x++) {
+                        regu += "<option value="+data[x].kd_regu + ">" + data[x].regu + "</option>"; // data json yang telah dioutput diassign ke variable dalam bentuk tag <option>
+                    }
+                    console.log(regu); // ini hanya untuk cek di console browser, apakah data berhasil teroutput?
+                    $("#kd_regu")
+                    .empty()
+                    .append(regu) // variable yang berisi tag <option> diassign ke combobox terkait
+                    .prop("disabled", false);
+                }
+            })
+        })
     </script>
 @endsection
