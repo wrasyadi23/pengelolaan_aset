4e3a244f73a6765564caf292c0d0580cef453d29
Henda Bastian <hendabast@gmail.com>
2021-03-01 13:25:31 +0700

    fix(laporan pekerjaan): fix query conditional clauses
    Took 1 hour 52 minutes

--- app/Http/Controllers/LaporanPekerjaanController.php
+++ app/Http/Controllers/LaporanPekerjaanController.php
@@ -26,22 +26,18 @@ class LaporanPekerjaanController extends Controller
         $kd_regu = $request->kd_regu;
 
         $seksi = Seksi::where('kd_bagian', Auth::user()->getKaryawan->kd_bagian)->get();
-        $pekerjaan = Pekerjaan::with('getKlasifikasi')->get()
-            ->groupBy('getKlasifikasi.kd_klasifikasi_pekerjaan');
 
-        if (isset($start) && isset($end)) {
-            $pekerjaan->whereBetween('tanggal_pekerjaan', [$start, $end]);
-        }
-
-        if (isset($kd_seksi)) {
-            $regu = Regu::where('kd_seksi', $kd_seksi)->get()->pluck('kd_regu');
-            $pekerjaan->whereIn('getKlasifikasi.kd_klasifikasi_pekerjaan', PekerjaanKlasifikasi::whereIn('kd_regu', $regu)->get()->pluck('kd_klasifikasi_pekerjaan'));
-        }
-
-        if (isset($kd_regu)) {
-            $pekerjaan->whereIn('getKlasifikasi.kd_klasifikasi_pekerjaan', PekerjaanKlasifikasi::where('kd_regu', $kd_regu)->get()->pluck('kd_klasifikasi_pekerjaan'));
-        }
-
+        $pekerjaan = Pekerjaan::with('getKlasifikasi')
+            ->when(isset($start) && isset($end), function ($query) use ($start, $end) {
+                return $query->whereBetween('tanggal_pekerjaan', [$start, $end]);
+            })
+            ->when(isset($kd_seksi), function ($query) use ($kd_seksi) {
+                $regu = Regu::where('kd_seksi', $kd_seksi)->get()->pluck('kd_regu');
+                $query->whereIn('kd_klasifikasi_pekerjaan', PekerjaanKlasifikasi::whereIn('kd_regu', $regu)->get()->pluck('kd_klasifikasi_pekerjaan'));
+            })
+            ->when(isset($kd_regu), function ($query) use ($kd_regu) {
+                $query->whereIn('kd_klasifikasi_pekerjaan', PekerjaanKlasifikasi::where('kd_regu', $kd_regu)->get()->pluck('kd_klasifikasi_pekerjaan'));
+            })->get()->groupBy('getKlasifikasi.kd_klasifikasi_pekerjaan');
 
         return view('/pemeliharaan/laporan', [
             'seksi' => $seksi,
