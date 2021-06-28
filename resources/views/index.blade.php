282995161ffa136b8b40026c07aecca3dd7d7bfe
wrasyadi23 <muhammad.rasyadi23@gmail.com>
2021-06-28 11:46:39 +0700

    pengumuman

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,28 @@
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-body">
+                        <h4 class="card-title">Pengumuman</h4>
+                        <div class="card-content">
+                            <div class="alert alert-danger">
+                                <h4 class="alert-heading">Perhatian!</h4>
+                                <p>
+                                    Mohon maaf jika ada keterlambatan pelayanan pekerjaan
+                                    karena kantor Pelayanan Umum dan Bagian Pemeliharaan Kawasan
+                                    sedang diberlakukan lockdown mulai 28 Juni - 04 Juli 2021
+                                </p>
+                                <p class="mb-0">
+                                    <b>
+                                        Pelayanan Umum
+                                    </b>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
                         <h4 class="card-title">Selamat Datang</h4>
                         <div class="card-content">
                             <p class="text-justify">
